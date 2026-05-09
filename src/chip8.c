be912d3593fafa3bba5cbbadd21131c4c7fa598e
deepak-pandey2005 <deepakpanday.in@gmail.com>
2026-05-09 17:17:16 +0530

    chore : created some opcodes

--- src/chip8.c
+++ src/chip8.c
@@ -1,6 +1,12 @@
 void chip8_init(Chip8 *chip8){
 
     memset(chip8,0,sizeof(Chip8)); //this sets the entire memory to 0
+    /* we can use memset to reset the memory to 0 or 
+       
+       for(int i = 0; i < 2048; ++i)
+		       gfx[i] = 0;
+
+       we can use like this for all other variables in the struct which we are using as a emulator information*/
 
     chip8->pc = 0x200; // 0x200-0xFFF - Program ROM and work RAM
 
@@ -49,4 +55,51 @@ void chip8_load_application(Chip8 *chip8 , const char *filename){
     // close file ,free buffer
     fclose(file);
     free(buffer);
+}
+
+void chip8_cycle(Chip8 *chip8)
+{
+  // fetch opcode
+  chip8->opcode = chip8->memory[pc] << 8 | memory[pc + 1]; 
+  /* as one opcode is two byte long we will need to fetch two
+   * succesive byte and merge them to get an actual opcode .*/
+
+  // decode opcode
+  switch (chip8->opcode & 0xF000)
+  {
+      case 0x1000:
+      {
+       uint16_t address = chip8->opcode & 0x0FFF;
+        chip8->pc = address;
+        break;
+      } /* this opcode implement the chip8 1NNN instruction which means jump to address NNN */
+      
+      case 0x2000:
+      {
+         chip8->stack[sp] = pc;
+         ++sp;
+         chip8->pc = chip8->opcode & 0x0FFF;
+         break;
+      } // this opcode calls the sub routine at address NNN
+
+      case 0x6000;
+      {
+       
+
+
+
+      }
+      
+
+
+
+
+
+
+
+
+  }
+  
+
+
 }
